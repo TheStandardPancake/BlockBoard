c2c7e2aed55daf24d23b01030f811465435b315e
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2021-03-10 16:10:36 +1100

    Making a main file for the interaction with the network and todo list

---
+++ BlockBoard.cpp
@@ -0,0 +1,28 @@
+//This is going to be the client interactive application for making a post and transfering funds.
+
+#include <sha256.h>
+/*
+TODO:
+-P2P connection network
+-Block creating --> binding both posts and transactions
+-Time stamp server
+-Code formating features for posts
+-mining system --> creating a tool for mining
+-creating a tool for viewing posts
+*/
+
+void p2p() {
+
+}
+
+
+int main() {
+  //Order of operations:
+
+  //Connect to the P2P
+
+  //Login through provision of your private key so as to sign posts
+
+  //Make transactions or make a post (a post has the inherent transaction of currency to even make the post)
+
+}
