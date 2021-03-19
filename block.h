e80f4cc43f08366a5253a3199d7dd156e10f5770
Boyd K <herobrine75@live.com>
2021-03-19 11:33:26 +1100

    Essentially completed the merkle tree hashing function, it is however yet to be tested.

--- block.h
+++ block.h
@@ -15,21 +15,41 @@ public:
     }
 
     int MerkleHash(string post, int transactions[]) {
+        int intialHashes[12];
+        int secondHashBatch[6];
+        int thirdHashBatch[3];
         int finalHash;
-        int intialHashes[];
 
+        //HASH 1: hash all of them using SHA256
+        //Add Initial
+        initialHashes[0] = picosha2::hash256_hex_string(post);
 
-        for (int i = 0; i <= 11; ++) {
-            cout <<
+        for (int i = 1; i < 12; i++) {
+            initialHashes[i] = picoshsa2::hash256_hex_string(transaction[i]);
         }
 
-        //HASH 1: hash all of them using SHA-256
-
         //pair up into pairs of 2, doesn't matter if a post goes with a transaction
+        for (int i = 0; i < 6; i++) {
+            secondHashBatch[i] = initialHashes[2*i]+initialHashes[2*i+1];
+        }
 
-        //HASH 2: Hash these pairs using SHA-2
+        //HASH 2: Hash these pairs using SHA256
+        for (int i = 0; i < 6; i++) {
+            secondHashBatch[i] = picosha2::hash256_hex_string(secondHashBatch[i]);
+        }
 
-        //continue pairing and hashing until one value is reached
+        //pair up into pairs again
+        for (int i = 0; i < 3; i++) {
+            thirdHashBatch[i] = secondHashBatch[2*i]+secondHashBatch[2*i+1];
+        }
+
+        //HASH 3: Hash these pairs using SHA256
+        for (int i = 0; i < 3; i++) {
+            thirdHashBatch[i] = picosha2::hash256_hex_string(thirdHashBatch[i]);
+        }
+
+        //FINAL HASH: Hash the three remainders together.
+        finalHash = picosha2::hash256_hex_string(thirdHashBatch[0]+thirdHashBatch[1]+thirdHashBatch[2])
 
         return finalHash;
     }
