b06bbc9dccb034ef64ee64c66570a6d12b70aea2
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2021-06-06 21:05:09 +1000

    Got a window opening for the gui

--- BlockBoard.cpp
+++ BlockBoard.cpp
@@ -30,142 +30,47 @@ TODO:
 -creating a tool for viewing posts
 */
 #include <windows.h>
-#include <stdlib.h>
-#include <string.h>
-#include <tchar.h>
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Turn the following into my own stuff------------<<<
 
-// The main window class name.
-static TCHAR szWindowClass[] = _T("BlockBoard");
 
-// The string that appears in the application's title bar.
-static TCHAR szTitle[] = _T("The Crypto Message Board");
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-HINSTANCE hInst;
-
-// Forward declarations of functions included in this code module:
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-int CALLBACK WinMain(
-   _In_ HINSTANCE hInstance,
-   _In_opt_ HINSTANCE hPrevInstance,
-   _In_ LPSTR     lpCmdLine,
-   _In_ int       nCmdShow
-)
+int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
-   WNDCLASSEX wcex;
+    WNDCLASSW wc = {0};
 
-   wcex.cbSize = sizeof(WNDCLASSEX);
-   wcex.style          = CS_HREDRAW | CS_VREDRAW;
-   wcex.lpfnWndProc    = WndProc;
-   wcex.cbClsExtra     = 0;
-   wcex.cbWndExtra     = 0;
-   wcex.hInstance      = hInstance;
-   wcex.hIcon          = LoadIcon(hInstance, IDI_APPLICATION);
-   wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-   wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-   wcex.lpszMenuName   = NULL;
-   wcex.lpszClassName  = szWindowClass;
-   wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
 
-   if (!RegisterClassEx(&wcex))
-   {
-      MessageBox(NULL,
-         _T("Call to RegisterClassEx failed!"),
-         _T("Windows Desktop Guided Tour"),
-         NULL);
 
-      return 1;
-   }
+    if(!RegisterClassW(&wc))
+        return -1;
 
-   // Store instance handle in our global variable
-   hInst = hInstance;
+    CreateWindowW(L"myWindowClass",L"Block Board", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 700, 500, NULL, NULL, NULL, NULL);
 
-   // The parameters to CreateWindow explained:
-   // szWindowClass: the name of the application
-   // szTitle: the text that appears in the title bar
-   // WS_OVERLAPPEDWINDOW: the type of window to create
-   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-   // 500, 100: initial size (width, length)
-   // NULL: the parent of this window
-   // NULL: this application does not have a menu bar
-   // hInstance: the first parameter from WinMain
-   // NULL: not used in this application
-   HWND hWnd = CreateWindow(
-      szWindowClass,
-      szTitle,
-      WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, CW_USEDEFAULT,
-      750, 500,
-      NULL,
-      NULL,
-      hInstance,
-      NULL
-   );
+    MSG msg = {0};
 
-   if (!hWnd)
-   {
-      MessageBox(NULL,
-         _T("Call to CreateWindow failed!"),
-         _T("Windows Desktop Guided Tour"),
-         NULL);
+    while(GetMessage(&msg,NULL,NULL,NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-      return 1;
-   }
-
-   // The parameters to ShowWindow explained:
-   // hWnd: the value returned from CreateWindow
-   // nCmdShow: the fourth parameter from WinMain
-   ShowWindow(hWnd,
-      nCmdShow);
-   UpdateWindow(hWnd);
-
-   // Main message loop:
-   MSG msg;
-   while (GetMessage(&msg, NULL, 0, 0))
-   {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
-   }
-
-   return (int) msg.wParam;
+    return 0;
 }
 
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
-   PAINTSTRUCT ps;
-   HDC hdc;
-   TCHAR greeting[] = _T("Hello, Windows desktop!");
-
-   switch (message)
-   {
-   case WM_PAINT:
-      hdc = BeginPaint(hWnd, &ps);
-
-      // Here your application is laid out.
-      // For this introduction, we just print out "Hello, Windows desktop!"
-      // in the top left corner.
-      TextOut(hdc,
-         5, 5,
-         greeting, _tcslen(greeting));
-      // End application-specific layout section.
-
-      EndPaint(hWnd, &ps);
-      break;
-   case WM_DESTROY:
-      PostQuitMessage(0);
-      break;
-   default:
-      return DefWindowProc(hWnd, message, wParam, lParam);
-      break;
-   }
-
-   return 0;
+    switch(msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hWnd,msg,wp,lp);
+    }
 }
